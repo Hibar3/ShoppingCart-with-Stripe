8f9f2ced8ef1d153be0f65d6a65e22b2a00ff7bb
Hibari_Khoo <jongxiankhoo@gmail.com>
2020-08-10 17:07:33 +0800

    Add checkout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,24 @@ import Cart from '../components/Cart'
 import CartSummary from '../components/CartSummary'
 import Products from '../components/Products'
 
+// @material-ui/core components
+import { Button } from '@material-ui/core'
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 const IndexPage: NextPage = () => {
+  const classes = useStyles();
   return (
     <Layout title="Shopping Cart Demo">
-     
+      
       <div>
+      <Link as={`/cartpage`} href="/cartpage">
+         <Button  className={classes.customButton}>
+            <ShoppingCartIcon />
+            <p>Checkout</p>
+        </Button>
+        
+      </Link>
       <Cart>
           <Products />
         </Cart>
@@ -17,5 +30,21 @@ const IndexPage: NextPage = () => {
     </Layout>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  customTitle: {
+    textAlign: "center",
+  },
+
+  customButton: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+}));
 
 export default IndexPage
